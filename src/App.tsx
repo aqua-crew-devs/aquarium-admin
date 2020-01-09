6950e73d5539e60405881a826ba1c62b871546e9
Qingyu Deng <bitdqy@hotmail.com>
2020-01-09 22:56:59 +0800

    feat: add root router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Container from "./components/Container";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Switch } from "antd";
+import Dashboard from "./pages/Dashboard";
+import ChannelManager from "./pages/ChannelManager";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Container>
+        <Switch>
+          <Route path="/">
+            <Dashboard></Dashboard>
+          </Route>
+          <Route path="/channel-manager">
+            <ChannelManager></ChannelManager>
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
   );
-}
+};
 
 export default App;
