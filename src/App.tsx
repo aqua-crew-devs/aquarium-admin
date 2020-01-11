1501f3ab9ad88b007944c8c026f0f0e4ee913cfe
Qingyu Deng <bitdqy@hotmail.com>
2020-01-11 15:29:32 +0800

    feat: add proxy to test server

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import "./App.css";
 import Container from "./components/Container";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Switch } from "antd";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import ChannelManager from "./pages/ChannelManager";
+import "antd/dist/antd.css";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Container>
         <Switch>
-          <Route path="/">
-            <Dashboard></Dashboard>
-          </Route>
           <Route path="/channel-manager">
             <ChannelManager></ChannelManager>
+          </Route>
+          <Route path="/">
+            <Dashboard></Dashboard>
           </Route>
         </Switch>
       </Container>
